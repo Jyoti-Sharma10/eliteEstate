7b20719c1315b8385f1526a0ebd965d10c241f0f
Jyoti <js10092000@gmail.com>
2023-12-24 22:47:43 +0530

    database connected

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongoDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
+};
+
+module.exports = mongoDB;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoDB = require("./db");
+mongoDB();
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+
+app.listen(4000, () => {
+    console.log("Server listening on port 4000");
 });
