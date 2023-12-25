9fd898e85745189c9a26958094e423a157dee8b3
Jyoti <js10092000@gmail.com>
2023-12-25 21:40:53 +0530

    salt added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const signupValidation = [
     body('username', 'Username must be at least 3 characters long').isLength({ min: 3 }),
@@ -18,15 +19,18 @@ const signup = async (req, res) => {
 
     try {
         // check whether if user already exist.
-        let user = await User.findOne({email: email});
+        let user = await User.findOne({username: username});
         if(user) {
-            return res.status(400).json({errors: 'Sorry a user with this email already exists!'});
+            return res.status(400).json({errors: 'Sorry a user with this username already exists!'});
         }
+
+        let salt = await bcrypt.genSalt(10);
+        let secPassword = await bcrypt.hash(password, salt);
 
         user = await User.create({
             username: username,
             email: email,
-            password: password,
+            password: secPassword,
         });
 
         res.json({success:true});
